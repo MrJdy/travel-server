0f32a6c393a3afcc638a7c56e0f3e171c39fd442
MrJdy <jdy95270@gmail.com>
2018-09-02 13:33:21 +0800

    修改node服务器配置

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # travel-server  
 在线访问地址（PC端把浏览器调成手机模拟器查看效果更佳） http://47.106.113.72:8888/travel/index.html#/
+##运行项目 
+1、首先你要安装 nodeJS 环境   
+2、cd travel-server  
+3、node Server.js / supervisor Server.js / pm2 start Server.js 三个命令根据您的情况任选一个即可  
+	 运行命令后即可启动项目  
+##项目源代码  
+请移步  https://github.com/MrJdy/Travel
--- Server.js
+++ Server.js
@@ -1,44 +1,44 @@
-var PORT = 8888; //
-var hostName = '0.0.0.0';
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var mine = require('./mine').types; //
-var path = require('path');
+const PORT = 9999
+const HOSTNAME = '127.0.0.1'
+var http = require('http')
+var url = require('url')
+var fs = require('fs')
+var mine = require('./mine').types
+var path = require('path')
 
 var server = http.createServer(function (request, response) {
-    var pathname = url.parse(request.url).pathname;
-    var realPath = path.join("webapp", pathname); //这里设置自己的文件名称;
+  var pathname = url.parse(request.url).pathname
+  var realPath = path.join("webapp", pathname) //这里设置自己的文件名称;
 
-    var ext = path.extname(realPath);
-    ext = ext ? ext.slice(1) : 'unknown';
-    fs.exists(realPath, function (exists) {
-        if (!exists) {
-            response.writeHead(404, {
-                'Content-Type': 'text/plain'
-            });
-
-            response.write("This request URL " + pathname + " was not found on this server.");
-            response.end();
+  var ext = path.extname(realPath)
+  ext = ext ? ext.slice(1) : 'unknown'
+  fs.exists(realPath, function (exists) {
+    if (!exists) {
+      response.writeHead(404, {
+        'Content-Type': 'text/plain'
+      })
+      response.write("This request URL " + pathname + " was not found on this server.")
+      response.end()
+    } else {
+      fs.readFile(realPath, "binary", function (err, file) {
+        if (err) {
+          response.writeHead(500, {
+            'Content-Type': 'text/plain'
+          })
+          response.end(err)
         } else {
-            fs.readFile(realPath, "binary", function (err, file) {
-                if (err) {
-                    response.writeHead(500, {
-                        'Content-Type': 'text/plain'
-                    });
-                    response.end(err);
-                } else {
-                    var contentType = mine[ext] || "text/plain";
-                    response.writeHead(200, {
-                        'Content-Type': contentType
-                    });
-                    response.write(file, "binary");
-                    response.end();
-                }
-            });
+          var contentType = mine[ext] || "text/plain"
+          response.writeHead(200, {
+            'Content-Type': contentType
+          })
+          response.write(file, "binary")
+          response.end()
         }
-    });
-});
-server.listen(PORT, hostName, function(){
-    console.log(`服务器运行在  http://${hostName}:${PORT}`);
-});
+      })
+    }
+  })
+})
+server.listen(PORT, HOSTNAME, function () {
+  console.log('您的项目已运行成功，点击下方地址即可访问')
+  console.log(`服务器运行在：http://${HOSTNAME}:${PORT}/travel/index.html`)
+})
